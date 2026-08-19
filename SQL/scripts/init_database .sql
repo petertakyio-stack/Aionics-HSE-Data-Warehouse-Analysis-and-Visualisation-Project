7e7a934d6114a1c439839cbcc9d772a3e94a6a93
Peter Takyi Ohemeng <petertakyio@gmail.com>
2026-08-19 19:08:06 +0000

    Add files via upload

---
+++ SQL/scripts/init_database .sql
@@ -0,0 +1,47 @@
+/*
+
+==========================================================
+ Create Database and Schemas
+ ==========================================================
+
+ Script Purpose:
+    This script creates a new database after checking if it already exists.
+    If it already exists, it is dropped and recreated. 
+    Additionally, the script create 3 schemas from the medallion data management approach
+    bronze, silver, and gold
+
+NOTE: Running this script will drop the entire 'AionicsDataWarehouse' if it exists.
+All data within it will be permanently deleted.
+*/
+
+
+
+-- Create Database for Aionics HSE Data Warehouse Project
+
+USE master;
+GO
+
+-- Drop and recreate the AionicsDataWarehouse database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'AionicsDataWarehouse')
+BEGIN
+    ALTER DATABASE AionicsDataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE AionicsDataWarehouse;
+END;
+GO
+
+-- Create database
+CREATE DATABASE AionicsDataWarehouse;
+GO
+
+USE AionicsDataWarehouse;
+GO
+
+-- Create the medallion schema for the project
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
